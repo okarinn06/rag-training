26bd78c7616614ccde9c69077d2520fc74844725
KhanhDang21 <qxdangvietanh@gmail.com>
2026-06-02 21:21:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2026 Dang Nguyen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY.
+#
+# See the GNU General Public License for more details.
+
+def main():
+    print("Hello from rag-training!")
+
+
+if __name__ == "__main__":
+    main()
